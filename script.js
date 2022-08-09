412c72ea07bb616efde2269a095d1feb34be3ee9
Christian Aquino <cjfaquino@gmail.com>
2022-08-08 21:23:03 -0700

    change haveRead value to checked

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function addBookToLibrary() {
     const title = document.getElementById("title").value;
     const author = document.getElementById('author').value;
     const pages = document.getElementById("pages").value;
-    const haveRead = document.getElementById("haveRead").value;
+    const haveRead = document.getElementById("haveRead").checked;
     const newBook = new Book(title, author, pages, haveRead);
     myLibrary.push(newBook);
     console.log(myLibrary);
