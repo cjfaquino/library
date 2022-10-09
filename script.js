bfc156d288c209cb134526a4361d399b6d8c36a4
Christian Aquino <cjfaquino@gmail.com>
2022-10-08 23:41:37 -0700

    change to single quotes

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ const library = (() => {
   let myLibrary = [];
   // test data
   const testTitles = [
-    "A book of books",
-    "THE Book",
-    "Just Another Book",
-    "Not Your Average Book",
-    "Not a Book",
-    "That Book",
-    "This Book",
+    'A book of books',
+    'THE Book',
+    'Just Another Book',
+    'Not Your Average Book',
+    'Not a Book',
+    'That Book',
+    'This Book',
   ];
   const testAuthors = [
-    "Author",
-    "Another Author",
-    "That Author",
-    "This Author",
-    "Plenty of Authors",
-    "Not an Author",
-    "No Author",
+    'Author',
+    'Another Author',
+    'That Author',
+    'This Author',
+    'Plenty of Authors',
+    'Not an Author',
+    'No Author',
   ];
   class Book {
     static test() {
@@ -42,18 +42,18 @@ const library = (() => {
   }
 
   //cache DOM
-  const form = document.getElementById("form");
-  const library = document.getElementById("library");
-  const labels = form.getElementsByTagName("label");
-  const inputs = form.getElementsByTagName("input");
+  const form = document.getElementById('form');
+  const library = document.getElementById('library');
+  const labels = form.getElementsByTagName('label');
+  const inputs = form.getElementsByTagName('input');
   const [title, author, pages, haveRead] = inputs;
-  const submit = document.getElementById("submit");
-  const add = document.getElementById("add");
+  const submit = document.getElementById('submit');
+  const add = document.getElementById('add');
 
   //bind events
-  submit.addEventListener("click", addBookToLibrary);
-  submit.addEventListener("click", render);
-  add.addEventListener("click", showForm);
+  submit.addEventListener('click', addBookToLibrary);
+  submit.addEventListener('click', render);
+  add.addEventListener('click', showForm);
 
   function getLibrary() {
     return myLibrary;
@@ -61,24 +61,24 @@ const library = (() => {
 
   function showForm() {
     for (const item of labels) {
-      item.classList.remove("hide");
+      item.classList.remove('hide');
     }
     for (const item of inputs) {
-      item.classList.remove("hide");
+      item.classList.remove('hide');
     }
-    submit.classList.remove("hide");
-    add.classList.add("hide");
+    submit.classList.remove('hide');
+    add.classList.add('hide');
   }
 
   function hideForm() {
     for (const item of labels) {
-      item.classList.add("hide");
+      item.classList.add('hide');
     }
     for (const item of inputs) {
-      item.classList.add("hide");
+      item.classList.add('hide');
     }
-    submit.classList.add("hide");
-    add.classList.remove("hide");
+    submit.classList.add('hide');
+    add.classList.remove('hide');
   }
 
   function addBookToLibrary() {
@@ -117,60 +117,60 @@ const library = (() => {
   }
 
   function setLocalStorage() {
-    localStorage.setItem("library", JSON.stringify(myLibrary));
+    localStorage.setItem('library', JSON.stringify(myLibrary));
   }
 
   function getLocalStorage() {
-    const local = JSON.parse(localStorage.getItem("library"));
+    const local = JSON.parse(localStorage.getItem('library'));
     myLibrary = local;
   }
 
   function createNewCardDOM(index) {
-    const card = document.createElement("div");
-    const content = document.createElement("div");
-    card.classList.add("card");
+    const card = document.createElement('div');
+    const content = document.createElement('div');
+    card.classList.add('card');
 
-    const preTextTitle = document.createElement("span");
-    const preTextAuthor = document.createElement("span");
-    const mainTextTitle = document.createElement("span");
-    const mainTextAuthor = document.createElement("span");
-    const pagesText = document.createElement("span");
-    const pagesNumber = document.createElement("span");
-    const cardImage = document.createElement("img");
-    const cardTitle = document.createElement("div");
-    const cardAuthor = document.createElement("div");
-    const cardPages = document.createElement("div");
-    const readLabel = document.createElement("label");
-    const readText = document.createTextNode("Read: ");
-    const cardHaveRead = document.createElement("input");
-    const deleteBtn = document.createElement("button");
+    const preTextTitle = document.createElement('span');
+    const preTextAuthor = document.createElement('span');
+    const mainTextTitle = document.createElement('span');
+    const mainTextAuthor = document.createElement('span');
+    const pagesText = document.createElement('span');
+    const pagesNumber = document.createElement('span');
+    const cardImage = document.createElement('img');
+    const cardTitle = document.createElement('div');
+    const cardAuthor = document.createElement('div');
+    const cardPages = document.createElement('div');
+    const readLabel = document.createElement('label');
+    const readText = document.createTextNode('Read: ');
+    const cardHaveRead = document.createElement('input');
+    const deleteBtn = document.createElement('button');
 
     card.appendChild(cardImage);
-    card.appendChild(content).classList.add("cardContent");
-    content.appendChild(cardTitle).classList.add("cardTitle");
-    cardTitle.appendChild(preTextTitle).classList.add("preText");
-    cardTitle.appendChild(mainTextTitle).classList.add("mainText");
-    cardAuthor.appendChild(preTextAuthor).classList.add("preText");
-    cardAuthor.appendChild(mainTextAuthor).classList.add("mainText");
-    content.appendChild(cardAuthor).classList.add("cardAuthor");
-    cardPages.appendChild(pagesNumber).classList.add("pagesNumber");
-    cardPages.appendChild(pagesText).classList.add("pagesText");
-    content.appendChild(cardPages).classList.add("cardPages");
-    content.appendChild(readLabel).classList.add("readLabel");
+    card.appendChild(content).classList.add('cardContent');
+    content.appendChild(cardTitle).classList.add('cardTitle');
+    cardTitle.appendChild(preTextTitle).classList.add('preText');
+    cardTitle.appendChild(mainTextTitle).classList.add('mainText');
+    cardAuthor.appendChild(preTextAuthor).classList.add('preText');
+    cardAuthor.appendChild(mainTextAuthor).classList.add('mainText');
+    content.appendChild(cardAuthor).classList.add('cardAuthor');
+    cardPages.appendChild(pagesNumber).classList.add('pagesNumber');
+    cardPages.appendChild(pagesText).classList.add('pagesText');
+    content.appendChild(cardPages).classList.add('cardPages');
+    content.appendChild(readLabel).classList.add('readLabel');
     readLabel.appendChild(readText);
-    readLabel.appendChild(cardHaveRead).classList.add("cardHaveRead");
-    card.appendChild(deleteBtn).classList.add("deleteBtn");
-    cardHaveRead.type = "checkbox";
+    readLabel.appendChild(cardHaveRead).classList.add('cardHaveRead');
+    card.appendChild(deleteBtn).classList.add('deleteBtn');
+    cardHaveRead.type = 'checkbox';
 
     cardImage.src = `https://picsum.photos/165/260?random=${index}`;
-    preTextTitle.textContent = "Title: ";
-    preTextAuthor.textContent = "By: ";
+    preTextTitle.textContent = 'Title: ';
+    preTextAuthor.textContent = 'By: ';
     mainTextTitle.textContent = myLibrary[index].title;
     mainTextAuthor.textContent = myLibrary[index].author;
-    pagesText.textContent = " pages";
+    pagesText.textContent = ' pages';
     pagesNumber.textContent = myLibrary[index].pages;
     cardHaveRead.checked = myLibrary[index].haveRead;
-    deleteBtn.textContent = "Delete";
+    deleteBtn.textContent = 'Delete';
     return { cardHaveRead, deleteBtn, card };
   }
 
@@ -180,8 +180,8 @@ const library = (() => {
     for (let i = 0; i < myLibrary.length; i++) {
       const { cardHaveRead, deleteBtn, card } = createNewCardDOM(i);
 
-      cardHaveRead.addEventListener("click", updateRead(i));
-      deleteBtn.addEventListener("click", deleteBook(i));
+      cardHaveRead.addEventListener('click', updateRead(i));
+      deleteBtn.addEventListener('click', deleteBook(i));
 
       library.appendChild(card);
     }
@@ -189,7 +189,7 @@ const library = (() => {
   return { render, Book, getLibrary, setLocalStorage, getLocalStorage };
 })();
 
-if (typeof localStorage.library === "undefined") {
+if (typeof localStorage.library === 'undefined') {
   library.Book.test();
   library.setLocalStorage();
 }
